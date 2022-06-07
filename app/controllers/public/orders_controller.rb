3f19aec1944fa065c6a4a1de5b51a8363b1c38d1
EC2 Default User <ec2-user@ip-172-31-46-87.ap-northeast-1.compute.internal>
2022-06-07 13:43:03 +0000

    [Finish]order/comfirm

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -21,7 +21,7 @@ class Public::OrdersController < ApplicationController
     if params[:order][:address] == "0"
     @order.postal_code = current_customer.postal_code
     @order.address = current_customer.address
-    @order.name = current_customer.name
+    @order.name = current_customer.full_name
     elsif params[:order][:address] == "1"
       @order.postal_code = Address.find(params[:order][:address_id]).postal_code
       @order.address = Address.find(params[:order][:address_id]).address
@@ -29,7 +29,7 @@ class Public::OrdersController < ApplicationController
     elsif params[:order][:address] == "2"
       @new_address = Address.new
       @new_address.postal_code = params[:order][:postal_code]
-      @new_address.address = params[:order][:address]
+      @new_address.address = params[:order][:new_address]
       @new_address.name = params[:order][:name]
       @new_address.customer_id = current_customer.id
       if @new_address.save
@@ -50,12 +50,15 @@ class Public::OrdersController < ApplicationController
     @order = Order.new(order_params)
     @order.customer_id = current_customer.id
     @order.save
-    @order_details = OrderDetail.new
     @cart_items = current_customer.cart_items
-    @order_details.item_id = @cart_item.item_id
-    @order_details.amount = @cart_item.amount
-    @order_details.price = @cart_item.item.add_tax_price
-    @order_details.save
+    @cart_items.each do |cart_item|
+      @order_details = OrderDetail.new
+      @order_details.order_id = @order.id
+      @order_details.item_id = cart_item.item_id
+      @order_details.amount = cart_item.amount
+      @order_details.price = cart_item.item.add_tax_price
+      @order_details.save
+    end
     current_customer.cart_items.destroy_all
     redirect_to complete_public_orders_path
   end
