0d620d3f747cc31106e325362e9e5955dcd5b263
EC2 Default User <ec2-user@ip-172-31-46-87.ap-northeast-1.compute.internal>
2022-06-09 15:38:48 +0000

    order/show

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -9,10 +9,11 @@ class Public::OrdersController < ApplicationController
 
   def index
     @orders = current_customer.orders
-    @items = current_customer.items
   end
 
   def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
   end
   
   def comfirm
